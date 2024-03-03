3a6d8486c667fae3568c934a717ea51de62b4d00
Pankaj Singh <p7ksingh@gmail.com>
2024-03-03 22:14:32 +0530

    example

---
+++ UserInputLinkedList.java
@@ -0,0 +1,38 @@
+package com.arr.Linked_List;
+
+import java.util.Scanner;
+
+public class UserInputLinkedList {
+    public static Node<Integer> input() {
+        Scanner sc = new Scanner(System.in);
+        int data = sc.nextInt();
+        Node<Integer> head = null;
+        while (data != -1) {
+            Node<Integer> currentNode = new Node<Integer>(data);
+            if (head == null) {
+                head = currentNode;
+            } else {
+                Node<Integer> tail = head;
+                while (tail.next != null) {
+                    tail = tail.next;
+                }
+                tail.next = currentNode;
+            }
+            data = sc.nextInt();
+        }
+        return head;
+    }
+
+    public static void print(Node<Integer> head) {
+        Node<Integer> temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node<Integer> head = input();
+        print(head);
+    }
+}
